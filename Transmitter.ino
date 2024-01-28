fb2ee55c5821bec47c333cfe6deb701ec8272285
Dutchy <72385882+JustADutchy@users.noreply.github.com>
2024-01-28 14:49:57 +0100

    Add files via upload

--- Transmitter.ino
+++ Transmitter.ino
@@ -10,9 +10,11 @@
 //===========================================
 // Definitions (What is the meaning of life?)
 //===========================================
-#define soilHumVCC 12  // Pin to enable power to soil humidity sensor
-#define soilHumPin 14  // Pin to read analog signal from soil humidity sensor
-#define DHTPIN 15      // Pin to read air temperature and humidity
+#define pin_Pump 8          // Pin to enable/disable pump
+#define pin_soilHumVCC 12   // Pin to enable power to soil humidity sensor
+#define pin_soilHumData 14  // Pin to read analog signal from soil humidity sensor
+#define pin_DHT 15          // Pin to read air temperature and humidity
+
 #define DHTTYPE DHT22  // Set DHT sensor type
 
 #define interval 2 * 1000  // Set interval in milliseconds and convert it to seconds
@@ -24,18 +26,20 @@
 //===========================================
 // Variables
 //===========================================
-int humidityAir;         // Store air humidity
-int temperatureAir;      // Store air temperature
+
+int humidityAir;           // Store air humidity
 int humiditySoil;          // Store soil humidity
 int maxHumiditySoil = 60;  // Maximum soil humidity, dont water if above this
 int minHumiditySoil = 35;  // Minimum soil himidity, water if below this
-bool soilIsWet;              // true if wet, false if dry
+float temperatureAir;      // Store air temperature
+bool soilIsWet;            // true if wet, false if dry
+bool DEBUG = false;
 
 
 //===========================================
 // Set up peripherals
 //===========================================
-DHT dht(DHTPIN, DHTTYPE);                                                   // Setup the DHT sensor
+DHT dht(pin_DHT, DHTTYPE);                                                  // Setup the DHT sensor
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1, 1000000);  // Setup the OLED
 
 
@@ -43,9 +47,9 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1, 1000000);  // S
 // Actually do things
 //===========================================
 void setup() {
-  pinMode(soilHumVCC, OUTPUT);  // Pin turns on soil humidity sensor
-  pinMode(soilHumPin, INPUT);   // Pin that reads data from soil humidity sensor
-  pinMode(DHTPIN, INPUT);       // Pin that reads data from Air humidity & Temp sensor
+  pinMode(pin_soilHumVCC, OUTPUT);  // Pin turns on soil humidity sensor
+  pinMode(pin_soilHumData, INPUT);  // Pin that reads data from soil humidity sensor
+  pinMode(pin_DHT, INPUT);          // Pin that reads data from Air humidity & Temp sensor
 
   Serial.begin(9600);   // Serial port to Computer
   Serial2.begin(9600);  // Serial port to HC12
@@ -58,10 +62,10 @@ void setup() {
 }
 
 void loop() {
-  getAllSensorData();
-  updateDisplay();
-  delay(interval);  // Replace delay with something smarter sometime
-  transmitData();
+  getAllSensorData();  // Kinda explains itself doesn't it?
+  updateDisplay();     // Updates the display with new sensor status (duh)
+  delay(interval);     // Replace delay with something smarter sometime
+  transmitData();      // Transmit new sensor data over HC12
 }
 
 
@@ -70,27 +74,33 @@ void loop() {
 //===========================================
 void getHumidityAir() {
   humidityAir = dht.readHumidity();  // Read current air humidity percentage
-  Serial.print("Humidity: ");        // Print to serial cuz why not
-  Serial.print(humidityAir);
-  Serial.println("%");
+  if (DEBUG) {
+    Serial.print("Humidity: ");  // Print to serial cuz why not
+    Serial.print(humidityAir);
+    Serial.println("%");
+  }
 }
 
 void getTemperatureAir() {
   temperatureAir = dht.readTemperature();  // Read current air temperature
-  Serial.print("Temperature:");            // Print to serial cuz why not
-  Serial.print(temperatureAir);
-  Serial.println(" Celsius");
+  if (DEBUG) {
+    Serial.print("Temperature:");  // Print to serial cuz why not
+    Serial.print(temperatureAir);
+    Serial.println(" Celsius");
+  }
 }
 
 void getHumiditySoil() {
-  digitalWrite(soilHumVCC, HIGH);  // Power the soil sensor ON
+  digitalWrite(pin_soilHumVCC, HIGH);  // Power the soil sensor ON
   delay(10);
-  humiditySoil = map(analogRead(soilHumPin), 200, 1000, 100, 0);  // Read current soil humidity
+  humiditySoil = map(analogRead(pin_soilHumData), 100, 1000, 100, 0);  // Read current soil humidity
   delay(10);
-  digitalWrite(soilHumVCC, LOW);   // Power the soil sensor OFF
-  Serial.print("Humidity Soil:");  // Print to serial cuz why not
-  Serial.print(humiditySoil);
-  Serial.println("%");
+  digitalWrite(pin_soilHumVCC, LOW);  // Power the soil sensor OFF
+  if (DEBUG) {
+    Serial.print("Humidity Soil:");  // Print to serial cuz why not
+    Serial.print(humiditySoil);
+    Serial.println("%");
+  }
   if (humiditySoil > 35) {
     soilIsWet = true;
   } else {
@@ -114,28 +124,26 @@ void updateDisplay() {     // Display ALL the sensor data
   display.print(humidityAir);
   display.print("%");
 
-  display.setCursor(0, 16);  // Update soil humidity on OLED
+  display.setCursor(0, 12);  // Update soil humidity on OLED
   display.print("Humid. soil:");
-  display.setCursor(80, 16);
+  display.setCursor(80, 12);
   display.print(humiditySoil);
   display.print("%");
 
-  display.setCursor(0, 32);  // Update temperature on OLED
+  display.setCursor(0, 24);  // Update temperature on OLED
   display.print("Temperature:");
-  display.setCursor(80, 32);
-  display.print(temperatureAir);
+  display.setCursor(80, 24);
+  display.print(temperatureAir, 1);
   display.print("c");
 
-  display.setCursor(0, 48);  // Update temperature on OLED
+  display.setCursor(0, 54);  // Update soil state on OLED
   display.print("Soil is  ");
-  display.setCursor(80, 48);
+  display.setCursor(50, 54);
   if (soilIsWet) {
     display.print("WET");
   } else {
     display.print("DRY");
   }
-  
-
   display.display();  // Display changes made
 }
 
@@ -145,5 +153,10 @@ void transmitData() {
   Serial2.print("Humidity Soil:");
   Serial2.println(humiditySoil);
   Serial2.print("Temperature Air:");
-  Serial2.println(temperatureAir);
+  Serial2.println(temperatureAir, 1);
+}
+
+void pumpControl() {
+  while (humiditySoil < minHumiditySoil) {
+  }
 }
